ea868d5f240a1a4c001878e02bc26939a0b545c3
94ju <janithherath94@gmail.com>
2019-06-11 22:44:34 +0530

    usubscribe

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit,OnDestroy {
   user: {id: number, name: string};
+  paramsSubscription:Subscription;
 
   constructor(private route:ActivatedRoute) { }
 
@@ -17,13 +19,16 @@ export class UserComponent implements OnInit {
       name:this.route.snapshot.params.name
     };
 
-    this.route.params.
+    this.paramsSubscription=this.route.params.
       subscribe(
         (para:Params)=>{
             this.user.id=para['id'];
             this.user.name=para['name'];
         }
       );
+  }
+  ngOnDestroy(){
+    this.paramsSubscription.unsubscribe();
   }
   
 
