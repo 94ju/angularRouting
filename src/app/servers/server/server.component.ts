8538208b16534c2a239070589a704ad74041757a
94ju <janithherath94@gmail.com>
2019-06-15 17:22:15 +0530

    child routes phase 1

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -14,7 +14,13 @@ export class ServerComponent implements OnInit {
   constructor(private serversService: ServersService,private router:Router,private route:ActivatedRoute) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    const id=+this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
+    this.route.params.subscribe(
+      (params:Params)=>{
+        this.server=this.serversService.getServer(+params['id']);
+      }
+    )
   }
   onReload(){
     this.router.navigate(['users'],{relativeTo:this.route});
